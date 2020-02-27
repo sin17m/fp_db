edda280893a161a4527fef2d692fde8ad4a7eecc
Aswin Singaram Natarajan <aswin.singaramnatarajan@csiro.au>
2020-02-27 16:16:39 +1100

    renamed file and accounted for duplicate scoresets

--- fp_db_extraction.R
+++ fp_db_extraction.R
@@ -56,7 +56,27 @@ datum <- datum %>%
   left_join(trait_ins) %>% 
   left_join(traits) %>% 
   left_join(att) %>% 
-  arrange(row, col) %>% 
-  spread(trait, value)
+  arrange(row, col, trait)
 
-write_csv(datum, "meredin_fp_database.csv")
+rm_col <- c("_id", "node_id","traitInstance_id","timestamp","gps_long","gps_lat","saved","trait_id")
+
+datum <- datum %>% 
+  select(-rm_col)
+
+dup_dat <- datum %>% 
+  group_by(node_id, trait) %>% 
+  mutate(dup = 1:n()) %>% 
+  mutate(trait_new = paste(trait, dup, sep="_")) %>% 
+  ungroup() %>% 
+  select(node_id, trait_new, value,row, col, plotname, unique_id, tos) %>% 
+  spread(trait_new, value)
+
+col_names_df <- data.frame(col_head = colnames(dup_dat)) %>%
+  mutate(names = col_head) %>% 
+  separate(names, paste0("n",1:6)) %>% 
+  arrange(n1,n2,n3,n4,n5) %>% 
+  filter(!col_head %in% c("row","col","node_id","plotname","unique_id","tos"))
+
+write_csv(datum, "meredin_fp_database_file_02_long.csv", na="")
+write_csv(dup_dat, "meredin_fp_database_file_02_wide.csv", na="")
+
